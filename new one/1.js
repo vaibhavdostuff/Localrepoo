66d567d10931eb78cab2a969b8ef6550226641a2
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-11-27 17:49:15 +0530

    strat

---
+++ new one/1.js
@@ -0,0 +1,37 @@
+//1
+
+let num1 = 50
+let num2 = 50.05
+
+let str1 = "Negi"
+let str2 = 'Bisht'
+
+console.log(num1)
+console.log(num2)
+console.log(str1)
+console.log(str2)
+
+
+//2
+
+// Declare a variable and initialize it
+// Global variable declaration
+
+let Name = "Apple";
+
+// Function definition
+function MyFunction() {
+
+    // Local variable declaration
+    let num = 10;
+
+    // Display the value of Global variable
+    console.log(Name);
+
+    // Display the value of local variable
+    console.log(num);
+}
+
+// Function call
+MyFunction();
+
