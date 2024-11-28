29c2faaf3440ea04cdcf1ad40b113c2c1fefc09f
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-11-28 22:27:24 +0530

    strat

--- new one/1.js
+++ new one/1.js
@@ -35,3 +35,51 @@ function MyFunction() {
 // Function call
 MyFunction();
 
+//3
+
+// Variable Declarations
+let x, y, sum;
+
+// Assign value to the variables
+x = 10;
+y = 30;
+
+// Use arithmetic operator to
+// add two numbers
+sum = x + y;
+
+console.l
+
+//4
+
+// Variable Declarations
+let x, num, sum;
+
+// Assign value to the variables
+x = 20;
+y = 30
+
+// Expression to divide a number
+num = x / 2;
+
+// Expression to add two numbers
+sum = x + y;
+
+console.log(num + "\n" + sum);
+
+//4
+
+// Variable Declarations
+let x, num, sum;
+
+// Assign value to the variables
+x = 20;
+y = 30
+
+// Expression to divide a number
+num = x / 2;
+
+// Expression to add two numbers
+sum = x + y;
+
+console.log(num + "\n" + sum);
